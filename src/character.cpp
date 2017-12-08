e7367b140ab8cdac0c81dba694fd2f1a05ab30e8
Xuan-vinh Tran <xuan-vinh.tran@mastertp>
2017-12-08 09:23:53 +0100

    adding Character Class

---
+++ src/character.cpp
@@ -0,0 +1,5 @@
+#include "character.h"
+
+character::character()
+{
+}
---
+++ src/character.h
@@ -0,0 +1,10 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+class character
+{
+public:
+    character();
+};
+
+#endif // CHARACTER_H
