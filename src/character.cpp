42f211d649bb0fd33adf0172720e7bfed5ac4dc0
Camille Farineau <cam.farineau@gmail.com>
2018-01-12 10:44:16 +0100

    Comment the character related files

--- src/character.cpp
+++ src/character.cpp
@@ -1,30 +1,59 @@
+/*
+**    TP CPE Lyon
+**    Copyright (C) 2018 Camille FARINEAU / Ahmed LOUDGHIRI / Xuan-Vinh TRAN
+**
+**    Video Games Module - Sauver le soldat Rohmer
+**
+**    character.cpp
+**    Handle the main character
+*/
+
 #include "character.h"
 
 /**************************************************************************\
- * Character:Character                                          *
+ * Character:Character                                                    *
 \**************************************************************************/
 Character::Character()
     : mesh_body(nullptr), mesh_head(nullptr), mesh_weapon(nullptr), body(nullptr), head(nullptr), weapon(nullptr)
 {
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::getReloading_cooldown
+ * Get the cooldown of the reloading
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 u32 Character::getReloading_cooldown() const
 {
     return reloading_cooldown;
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::getHealth_point
+ * Get health point
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 u32 Character::getHealth_point() const
 {
     return health_point;
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::addCharacterMeshToScene
+ * Add the meshes to the scene and set the correct position
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::addCharacterMeshToScene(is::ISceneManager *smgr, std::vector<iv::ITexture*> textures){
 
-
+    // Add the meshes to the scene and add the correct texture - for all the part of the main character
     mesh_body = smgr->getMesh("../data/Chaingunner/chaingunner_body.md2");
     mesh_weapon = smgr->getMesh("../data/Chaingunner/chaingunner_weapon.md2");
     mesh_head = smgr->getMesh("../data/Chaingunner/chaingunner_head.md2");
     mesh_mf = smgr->getMesh("../data/Chaingunner/chaingunner_mf.md2");
+    // The weapon, head and mf are children of the body node. They will all move as the body move
     body = smgr->addAnimatedMeshSceneNode(mesh_body);
     weapon = smgr->addAnimatedMeshSceneNode(mesh_weapon,body,-1,core::vector3df(0,0,0),core::vector3df(0,0,0));
     head = smgr->addAnimatedMeshSceneNode(mesh_head,body,-1,core::vector3df(0,0,0),core::vector3df(0,0,0));
@@ -40,13 +69,20 @@ void Character::addCharacterMeshToScene(is::ISceneManager *smgr, std::vector<iv:
     head->setMaterialTexture(0, textures[2]);
     mf->setMaterialTexture(0, textures[4]);
 
+    // mf is corresponding to the mesh of the explosion at the end of the weapon
     mf->setVisible(false);
 
+    // Set the correct position and rotation for the main_character
     body->setPosition(core::vector3df(-1596.96, 95.7503, -983.472));
-    //body->setPosition(core::vector3df(-79, 156, 265));
     body->setRotation(core::vector3df(0.0, 0.0, 0.0));
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::addCharacterCollider
+ * Add the collider to the node of the main character
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::addCharacterCollider(is::ISceneManager *smgr, scene::ITriangleSelector *selector){
     const core::aabbox3d<f32>& box = body -> getBoundingBox();
     core::vector3df radius = 1.2*(box.MaxEdge - box.getCenter());
@@ -56,12 +92,18 @@ void Character::addCharacterCollider(is::ISceneManager *smgr, scene::ITriangleSe
                                                   radius, // "rayons" de la caméra
                                                   ic::vector3df(0, -10, 0),  // gravité
                                                   ic::vector3df(0, -32, 0));  // décalage du centre
-
     body->addAnimator(anim);
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::setAnimation
+ * Set the proper animation (frame loop and speed) depending on the context
+ *  Param: anim
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::setAnimation(Animation anim)
 {
+    // Different animation depending on the context
     switch(anim)
     {
     case RUN:
@@ -108,29 +150,62 @@ void Character::setAnimation(Animation anim)
     }
 }
 
+/*------------------------------------------------------------------------*\
+ * EventReceiver::change_texture_weapon_fire
+ * Change the texture of the weapon when the character is attacking
+ *  Param: textures
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::change_texture_weapon_fire(std::vector<iv::ITexture*>& textures)
 {
     weapon->setMaterialTexture(0, textures[3]);
 }
+/*------------------------------------------------------------------------*\
+ * EventReceiver::change_texture_weapon_rest
+ * Change the texture of the weapon when the character is not attacking
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::change_texture_weapon_rest(std::vector<iv::ITexture*>& textures)
 {
     weapon->setMaterialTexture(0, textures[1]);
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::get_nb_stock
+ * Get the number of bullet in stock
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 u32 Character::get_nb_stock() const
 {
     return stock;
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::get_nb_munition
+ * Change the number of munition in the cartridge
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 u32 Character::get_nb_munition() const
 {
     return munition;
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::use_munition
+ * Decrement the number of ammunition
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::use_munition()
 {
     munition--;
 }
+/*------------------------------------------------------------------------*\
+ * EventReceiver::reload
+ * Function to reload : update the number of ammo in cartridge and in stock
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::reload()
 {
 
@@ -146,7 +221,12 @@ void Character::reload()
         reloading_cooldown = 45;
     }
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::is_reloading
+ * Update the cooldown for the reloading phase : to reload it will take some frame
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 bool Character::is_reloading()
 {
     if(reloading_cooldown>0){
@@ -157,7 +237,13 @@ bool Character::is_reloading()
     return false;
 
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::is_invincible
+ * Set the correct textures and decrement the counter for the invincibility frame
+ * After the character has been hit there is a few frame where it is unvunerable
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::is_invincible(std::vector<iv::ITexture*>& textures)
 {
     if(invincibility_frame==1){
@@ -173,7 +259,12 @@ void Character::is_invincible(std::vector<iv::ITexture*>& textures)
 
 
 }
-
+/*------------------------------------------------------------------------*\
+ * EventReceiver::take_damage
+ * Update the health point of the character if it has been hit
+ *  Param:
+ *  Return :
+\*------------------------------------------------------------------------*/
 void Character::take_damage()
 {
     if (health_point == 1 && invincibility_frame == 0){
